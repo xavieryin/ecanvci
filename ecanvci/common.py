8be5481aa5dbe188f3079e30e12f51e341fb4fa9
xavieryin <bug313@gmail.com>
2022-03-18 15:06:06 +0800

    v1.3.0 - support all known timings from 5 ~ 1000 kbps

--- ecanvci/common.py
+++ ecanvci/common.py
@@ -1,10 +1,30 @@
 from ctypes import Structure, LittleEndianStructure, memmove, addressof, sizeof
+import sys
 
+ECANVCI_TIMING = {5: (0xBF, 0xFF),
+                  10: (0x31, 0x1C),
+                  20: (0x18, 0x1C),
+                  40: (0x87, 0xFF),
+                  50: (0x09, 0x1C),
+                  80: (0x83, 0xFF),
+                  100: (0x04, 0x1C), 
+                  125: (0x03, 0x1C), 
+                  200: (0x81, 0xFA), 
+                  250: (0x01, 0x1C),
+                  400: (0x80, 0xFA), 
+                  500: (0x00, 0x1C),
+                  666: (0x80, 0xB6), 
+                  800: (0x00, 0x16), 
+                  1000: (0x00, 0x14)}
 
-ECANVCI_TIMING0_100KBPS = 0x04
-ECANVCI_TIMING1_100KBPS = 0x1C
-ECANVCI_TIMING0_250KBPS = 0x01
-ECANVCI_TIMING1_250KBPS = 0x1C
+# ECANVCI_TIMING0_100KBPS, ECANVCI_TIMING1_100KBPS = ECANVCI_TIMING[100]
+# ECANVCI_TIMING0_250KBPS, ECANVCI_TIMING1_250KBPS = ECANVCI_TIMING[250]
+# ECANVCI_TIMING0_500KBPS, ECANVCI_TIMING1_500KBPS = ECANVCI_TIMING[500]
+
+this_module = sys.modules[__name__]
+for kbps, timings in ECANVCI_TIMING.items():
+    setattr(this_module, f"ECANVCI_TIMING0_{kbps}KBPS", timings[0])
+    setattr(this_module, f"ECANVCI_TIMING1_{kbps}KBPS", timings[1])
 
 
 class StructureEx(Structure):
